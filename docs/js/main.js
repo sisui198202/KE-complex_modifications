891474c25f1635cc721df082f3b981e659df1735
sisui <avenuecafe1982@yahoo.co.jp>
2025-12-18 16:52:02 +0900

    a

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,7 +3,9 @@ $(function() {
   $('a[data-json-path]').each(function() {
     var $this = $(this);
     var json_path = $this.data('json-path');
-    var json_url = encodeURIComponent(location.href.replace(/[^/]+$/, '') + json_path);
+    var base = location.href.replace(/[^/]+$/, '');
+    var raw = base + json_path + '?t=' + new Date().getTime(); // キャッシュバスター
+    var json_url = encodeURIComponent(raw);
     var url = 'karabiner://karabiner/assets/complex_modifications/import?url=' + json_url;
     $this.attr('href', url);
   });
